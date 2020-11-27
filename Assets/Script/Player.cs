96b4237c902a3cc85cfbf63ce77d72fcf44c89ee
Suvamjyoti_Panda <likun54@gmail.com>
2020-11-27 09:36:23 +0530

    Smoothing Movement And Turn

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField]private float m_moveSpeed;
+    [SerializeField]private float m_moveSpeed=5f;
+    [SerializeField]private float m_smoothMoveTime=0.2f;
+    [SerializeField]private float m_turnSpeed=5f;
 
     private float m_vertical;
     private float m_horizontal;
+
+    private float m_currentAngle;
+    private float m_smoothInputMagnitude;
+    private float m_smoothMoveVelocity;
 
 //`````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````
 //`````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````
@@ -18,20 +24,22 @@ public class Player : MonoBehaviour
         m_horizontal = Input.GetAxisRaw("Horizontal");
 
         PlayerMovement();
-
     }
 
 //`````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````
 //`````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````
 
     private void PlayerMovement(){
+
         Vector3 _moveDirection = new Vector3(m_horizontal,0,m_vertical).normalized;
         float _inputMagnitude = _moveDirection.magnitude;
+        m_smoothInputMagnitude = Mathf.SmoothDamp(m_smoothInputMagnitude,_inputMagnitude,ref m_smoothMoveVelocity,m_smoothMoveTime);
 
         float _targetAngle = Mathf.Atan2(_moveDirection.x,_moveDirection.z)*Mathf.Rad2Deg;
-        transform.eulerAngles = Vector3.up * _targetAngle;
+        m_currentAngle = Mathf.LerpAngle(m_currentAngle,_targetAngle,Time.deltaTime * m_turnSpeed * _inputMagnitude);
+        transform.eulerAngles = Vector3.up * m_currentAngle;
 
-        transform.Translate(transform.forward*m_moveSpeed*_inputMagnitude*Time.deltaTime,Space.World);
+        transform.Translate(transform.forward * m_moveSpeed * m_smoothInputMagnitude * Time.deltaTime,Space.World);
     }
 
 }
